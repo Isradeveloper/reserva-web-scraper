74c25682b52a70330de8add3c0be3d57a71032a1
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2025-03-02 20:53:22 -0500

    Update mailer-send.controller.ts

--- src/mailer-send/mailer-send.controller.ts
+++ src/mailer-send/mailer-send.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from "@nestjs/common";
 import { MailerSendService } from "./mailer-send.service";
+import * as fs from "fs";
+import * as path from "path";
 
 @Controller("mailer-send")
 export class MailerSendController {
@@ -7,6 +9,12 @@ export class MailerSendController {
 
   @Get()
   async test() {
+    const filePath = path.join("reservas/1007118795/hola.txt");
+    const fileContent = "Hello, this is a test file.";
+
+    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    fs.writeFileSync(filePath, fileContent);
+
     await this.mailerSendService.sendMail({
       to: "theisratruji@gmail.com",
       subject: "Test",
